cc9f6e17ad1e4899a963a9e082b9b8c88386a28c
Peng Guo <errrrrricg@gmail.com>
2024-03-04 12:25:27 -0500

    sequence explode and fragment animation

--- Assets/PlayerCollisionInteraction/PCI_Explode.cs
+++ Assets/PlayerCollisionInteraction/PCI_Explode.cs
@@ -1,6 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
+using Random = UnityEngine.Random;
 
 public class PCI_Explode : PlayerCollisionInteraction
 {
@@ -12,31 +17,63 @@ public class PCI_Explode : PlayerCollisionInteraction
     [SerializeField] private float fragmentMass = 0.1f;
     [SerializeField,Header("Average disappear time")] private float fragmentDisappearTime = 10f;
     [SerializeField,Header("Difference between max and min disappear time")] private float fragmentDisappearTimeVariation = 1f;
-    
+    [SerializeField] private List<PCI_Explode> explodeBySequence = new List<PCI_Explode>();
+        
+    private Dictionary<PCI_Explode, bool> explodeSequenceTrack = new Dictionary<PCI_Explode, bool>();
+
+    private UnityEvent<PCI_Explode> OnExplosionTrigger = new UnityEvent<PCI_Explode>();
+
+    private void Awake()
+    {
+        if (explodeBySequence.Count != 0)
+        {
+            foreach (var VARIABLE in explodeBySequence)
+            {
+                explodeSequenceTrack.Add(VARIABLE,false);
+                VARIABLE.OnExplosionTrigger.AddListener(CheckExplosion);
+            }
+        }
+    }
+
+    public void CheckExplosion(PCI_Explode orgExplosion)
+    {
+        if (explodeSequenceTrack.ContainsKey(orgExplosion))
+        {
+            explodeSequenceTrack[orgExplosion] = true;
+            foreach (var VARIABLE in explodeSequenceTrack)
+            {
+                if(VARIABLE.Value == false) return;
+            }
+            PlayEffect();
+        }
+    }
+
     protected override void PlayEffect()
     {
         regularStateGO.SetActive(false);
         explosionStateGO.SetActive(true);
         Explode();
+        OnExplosionTrigger.Invoke(this);
         base.PlayEffect();
     }
 
     private void Explode()
     {
-        
         foreach (Transform go in explosionStateGO.transform)
         {
             Rigidbody rb = go.gameObject.AddComponent<Rigidbody>();
             rb.mass = fragmentMass * Random.Range(0.8f,1.2f);
             rb.AddExplosionForce(explosionForce, explosionOrigion.position,explosionRadius);
             go.gameObject.AddComponent<BoxCollider>();
-            StartCoroutine(DelayAddCollider(go.gameObject));
+            StartCoroutine(DelayDisappear(go.gameObject));
         }
     }
-    
-    IEnumerator DelayAddCollider(GameObject targetGO)
+
+    IEnumerator DelayDisappear(GameObject targetGO)
     {
         yield return new WaitForSeconds(fragmentDisappearTime += fragmentDisappearTimeVariation* Random.Range(-0.5f,0.5f));
+        targetGO.transform.DOScale(new Vector3(0.01f, 0.01f, 0.01f), 1f);
+        yield return new WaitForSeconds(1f);
         targetGO.SetActive(false);
     }
 }
