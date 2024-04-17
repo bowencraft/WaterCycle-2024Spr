9a527c5e7c082a87fc5b5e21123265c6b468552f
Peng Guo <errrrrricg@gmail.com>
2024-04-17 12:29:11 -0400

    add fatal water

---
+++ Assets/PlayerCollisionInteraction/PCI_FatalWater.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCI_FatalWater : PlayerCollisionInteraction
+{
+    [SerializeField] private float bufferTime = 2f;
+    [SerializeField] private float currentTime = 0f;
+    
+    protected override void PlayEffect()
+    {
+        print("player death");
+        base.PlayEffect();
+    }
+
+}
